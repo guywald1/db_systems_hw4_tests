dbab04a3051dac95aa32e8de7d31438532a4452c
Guy Waldman <guywald1@gmail.com>
2018-01-16 01:13:56 +0200

    Add test script

---
+++ test/script.test.js
@@ -0,0 +1,35 @@
+const conn = new Mongo();
+const db = conn.getDB("ooh-la-tech");
+
+// load mock data
+load("test/lib/mockData.js");
+// load test utils
+load("test/lib/testUtils.js");
+
+// -------------------------- MAIN -------------------------
+
+// execute script
+load("src/script.js");
+
+test("init #1", "all mock data was inserted", expect => {
+  assertEqualsCollectionArray("recipes", RECIPES, ["r_name"])(expect);
+  assertEqualsCollectionArray("costs", COSTS, ["i_name"])(expect);
+});
+
+test("#1", "allRecipesWithMixer #1", expect => {
+  assertEqualsCollectionArray("results1", RESULTS1, ["r_name"], query =>
+    query.sort({ r_name: 1 })
+  )(expect);
+});
+
+test("#2", "calcIngredientsUsage #1", expect => {
+  assertEqualsCollectionArray("results2", RESULTS2, ["_id", "value"], query =>
+    query.sort({ _id: 1 })
+  )(expect);
+});
+
+test("#3", "calcCosts #1", expect => {
+  assertEqualsCollectionArray("results3", RESULTS3, ["r_name", "cost"], query =>
+    query.sort({ r_name: 1 })
+  )(expect);
+});
