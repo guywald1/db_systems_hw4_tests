72e0fcac348776f9c9984838d9e0ffdf05a95109
Guy Waldman <guywald1@gmail.com>
2018-01-16 02:21:32 +0200

    Add showExpected script

---
+++ test/lib/showExpected.js
@@ -0,0 +1,16 @@
+load("test/lib/mockData.js")
+
+print("results1:\n -----")
+RESULTS1.forEach(result => {
+  printjson(result);
+})
+
+print("\nresults2:\n -----")
+RESULTS2.forEach(result => {
+  printjson(result);
+})
+
+print("\nresults3:\n -----")
+RESULTS3.forEach(result => {
+  printjson(result);
+})
